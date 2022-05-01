732be0a93b13e0fe2c9e0ba2fcfadd6c6a7cf671
hktaisei <mtsaihokutann17@outlook.jp>
2022-05-02 05:59:49 +0900

    first commit

---
+++ src/main/java/com/mainapp/hbootstrapspringtest/BootstrapSpringTestApplication.java
@@ -0,0 +1,13 @@
+package com.mainapp.hbootstrapspringtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootstrapSpringTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootstrapSpringTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mainapp/hbootstrapspringtest/BootstrapSpringTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mainapp.hbootstrapspringtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootstrapSpringTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
